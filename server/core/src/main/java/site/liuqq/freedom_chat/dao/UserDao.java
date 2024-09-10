d036d7252ef23504ac5e22fe548bdd62d9a7796f
677 <945491917@qq.com>
2024-09-10 17:24:59 +0800

    清理无用代码

--- server/core/src/main/java/site/liuqq/freedom_chat/dao/UserDao.java
+++
@@ -1,4 +0,0 @@
-package site.liuqq.freedom_chat.dao;
-
-public interface UserDao {
-}
--- server/core/src/main/java/site/liuqq/freedom_chat/dao/impl/UserDaoImpl.java
+++
@@ -1,9 +0,0 @@
-package site.liuqq.freedom_chat.dao.impl;
-
-import org.springframework.stereotype.Repository;
-import site.liuqq.freedom_chat.dao.UserDao;
-
-@Repository
-public class UserDaoImpl implements UserDao {
-
-}
